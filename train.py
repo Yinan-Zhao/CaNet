87383b017c42bca89692c4fe5916073c7ab5dee5
Zhao <alexzhao@cs.utexas.edu>
2020-01-30 15:30:14 -0600

    Initial commit

--- train.py
+++ train.py
@@ -100,7 +100,8 @@ cudnn.enabled = True
 model = Res_Deeplab(num_classes=num_class)
 #load resnet-50 preatrained parameter
 model = load_resnet50_param(model, stop_layer='layer4')
-model=nn.DataParallel(model,[2,3])
+model.cuda()
+#model=nn.DataParallel(model,[2,3])
 
 # disable the  gradients of not optomized layers
 turn_off(model)
